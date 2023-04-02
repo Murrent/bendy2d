25e92449e9c70a9932e3228acb3137046e651e3e
Tempron <wickgrenm@gmail.com>
2023-04-02 23:23:13 +0200

    Added function body to solve_circle

--- circle.rs
+++ circle.rs
@@ -8,7 +8,6 @@ pub struct Circle {
 }
 
 impl Circle {
-
     pub fn solve_bounds(&mut self, bounds: &Bounds) {
         if self.point.pos.x < bounds.pos.x + self.radius {
             self.point.pos.x = bounds.pos.x + self.radius;
@@ -23,6 +22,17 @@ impl Circle {
     }
 
     pub fn solve_circle(&mut self, circle: &mut Circle) {
-
+        let dist = self.point.pos - circle.point.pos;
+        let dist_sqr = dist.length_squared();
+        let radius_sum = self.radius + circle.radius;
+        if dist_sqr < radius_sum * radius_sum  {
+            let normal = dist.normalise();
+            let overlap = radius_sum - dist_sqr.sqrt();
+            let self_rad_sqr = self.radius * self.radius;
+            let circle_rad_sqr = circle.radius * circle.radius;
+            let scale = 1.0 / (self_rad_sqr + circle_rad_sqr);
+            self.point.pos += normal * scale  * overlap * circle_rad_sqr;
+            circle.point.pos -= normal * scale  * overlap * self_rad_sqr;
+        }
     }
 }
