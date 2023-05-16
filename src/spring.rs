eaf0a65f86ec4ddbca49714a5ddb85a58dcc446b
Tempron <wickgrenm@gmail.com>
2023-05-16 03:23:09 +0200

    Added permanent deformations

--- src/spring.rs
+++ src/spring.rs
@@ -6,19 +6,21 @@ pub struct Spring {
     pub particle_b: usize,
     pub rest_length: f32,
     pub stiffness: f32,
+    pub permanence_threshold: f32,
 }
 
 impl Spring {
-    pub fn new(particle_a: usize, particle_b: usize, rest_length: f32, stiffness: f32) -> Self {
+    pub fn new(particle_a: usize, particle_b: usize, rest_length: f32, stiffness: f32, permanence_threshold: f32) -> Self {
         Self {
             particle_a,
             particle_b,
             rest_length,
             stiffness,
+            permanence_threshold,
         }
     }
 
-    pub fn solve(&self, particles: &mut Vec<Particle>, dt: f32) {
+    pub fn solve(&mut self, particles: &mut Vec<Particle>, dt: f32) {
         let split = particles.split_at_mut(self.particle_b);
         let particle_a = &mut split.0[self.particle_a];
         let particle_b = &mut split.1[0];
@@ -30,7 +32,12 @@ impl Spring {
         }
         let delta_normal = delta / delta_length;
 
-        let difference = (delta_length - self.rest_length) / delta_length;
+        let mut difference = (delta_length - self.rest_length) / delta_length;
+        // Permanent deformation
+        if difference < self.permanence_threshold {
+            self.rest_length = delta_length;
+            difference = (delta_length - self.rest_length) / delta_length;
+        }
         let impulse = delta_normal * difference * self.stiffness;
 
         particle_a.pos += impulse * dt;
