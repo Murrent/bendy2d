549e65fbec1582c3792d29c828ad25238fb695b1
Tempron <wickgrenm@gmail.com>
2023-08-08 23:31:02 +0200

    Fixed projection and added is_point_in_polygon function

--- src/common.rs
+++ src/common.rs
@@ -28,19 +28,16 @@ pub fn line_intersection(
 
 #[inline]
 pub fn proj_a_on_b(a: Vector2<f32>, b: Vector2<f32>) -> Vector2<f32> {
-    (a.dot(&b) / b.magnitude_squared()) * b
+    let scalar_proj = a.dot(&b) / b.magnitude();
+    scalar_proj * b.normalize()
 }
 
 #[inline]
 pub fn proj_a_on_b_clamped(a: Vector2<f32>, b: Vector2<f32>) -> Vector2<f32> {
-    let proj = proj_a_on_b(a, b);
-    if proj.magnitude_squared() > b.magnitude_squared() {
-        b
-    } else if proj.magnitude_squared() < 0.0 {
-        Vector2::zeros()
-    } else {
-        proj
-    }
+    let b_magnitude = b.magnitude();
+    let scalar_proj = a.dot(&b) / b_magnitude;
+    let scalar_proj_clamped = scalar_proj.max(0.0).min(b_magnitude);
+    scalar_proj_clamped * b.normalize()
 }
 
 #[inline]
@@ -82,4 +79,38 @@ pub fn aabb_vs_aabb(bounds1: Bounds, bounds2: Bounds) -> bool {
         && bounds1.pos.x + bounds1.size.x > bounds2.pos.x
         && bounds1.pos.y < bounds2.pos.y + bounds2.size.y
         && bounds1.pos.y + bounds1.size.y > bounds2.pos.y
+}
+
+#[inline]
+pub fn is_point_in_polygon(point: &Vector2<f32>, polygon: &[Vector2<f32>]) -> Option<Vector2<f32>> {
+    let mut is_inside = false;
+    let mut closest_point = None;
+    let mut min_distance = f32::MAX;
+    let (mut j, mut i) = (polygon.len() - 1, 0);
+
+    while i < polygon.len() {
+        if ((polygon[i].y > point.y) != (polygon[j].y > point.y))
+            && (point.x < (polygon[j].x - polygon[i].x) * (point.y - polygon[i].y)
+            / (polygon[j].y - polygon[i].y)
+            + polygon[i].x)
+        {
+            is_inside = !is_inside;
+        }
+
+        let curr_point = proj_point_on_line(*point, (polygon[i], polygon[j]));
+        let curr_distance = (curr_point - *point).magnitude();
+        if curr_distance < min_distance {
+            min_distance = curr_distance;
+            closest_point = Some(curr_point);
+        }
+
+        j = i;
+        i += 1;
+    }
+
+    if is_inside {
+        closest_point
+    } else {
+        None
+    }
 }
